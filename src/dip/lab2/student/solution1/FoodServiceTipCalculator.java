9a5fd89f73fe9a841c949e1b84b7181fecd4ba5d
lhendrickson3@my.wctc.edu <>
2013-09-12 17:09:25 -0500

    Created BaggageServiceTipCalculator and FoodServiceTipCalculator as low lvl classes, CalculatorService as the high lvl class and TipCalculator as the abstraction layer (interface)

---
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author User
+ */
+public class FoodServiceTipCalculator implements TipCalculator{
+
+    @Override
+    public double getTip() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ src/dip/lab2/student/solution1/TipCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author User
+ */
+public interface TipCalculator {
+    
+    public static final double GOOD_RATE = 0.20;
+    public static final double FAIR_RATE = 0.15;
+    public static final double POOR_RATE = 0.10;
+    
+    public double getTip();
+    
+}
