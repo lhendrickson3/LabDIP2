702c153fbc0f4cfd93ba1fea9f9518fb011255fd
lhendrickson3@my.wctc.edu <>
2013-09-16 15:43:54 -0500

    9-16-2013 3:43

--- src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
@@ -9,10 +9,75 @@ package dip.lab2.student.solution1;
  * @author User
  */
 public class BaggageServiceTipCalculator implements TipCalculator{
+    private static final double MIN_BILL = 0.00;
+    private static final double MAX_BILL = 100.00;
+    private static final int ONE = 1;
+    private static final String FIRST = "bag count must be greater than or equal to zero";
+    private static final String BILL_ENTRY_ERR =
+            "Error: bill must be between " + MIN_BILL + " and "
+            + MAX_BILL;
+    private double baseTipPerBag;
+    private int bagCount;
+    public enum ServiceQuality {
+        GOOD, FAIR, POOR
+    }
+    private ServiceQuality serviceQuality;
 
-    @Override
-    public double getTip() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public BaggageServiceTipCalculator(ServiceQuality q, int bags) {
+        this.setServiceRating(q); // perform validation
+        this.setBagCount(bags);
+
+        baseTipPerBag = 1.00; // set default value
     }
     
+    @Override
+    public double getTip() {
+        double tip = 0.00; // always initialize local variables
+
+        switch(serviceQuality) {
+            case GOOD:
+                tip = baseTipPerBag * bagCount * (ONE + GOOD_RATE);
+                break;
+            case FAIR:
+                tip = baseTipPerBag * bagCount * (ONE + FAIR_RATE);
+                break;
+            case POOR:
+                tip = baseTipPerBag * bagCount * (ONE + POOR_RATE);
+                break;
+        }
+
+        return tip;
+    }
+    public final void setServiceRating(ServiceQuality q) {
+        // No need to validate because enums provide type safety!
+        serviceQuality = q;
+    }
+
+    public ServiceQuality getServiceQuality() {
+        return serviceQuality;
+    }
+
+    public int getBagCount() {
+        return bagCount;
+    }
+
+    public final void setBagCount(int bagCount) {
+        if(bagCount < 0) {
+            throw new IllegalArgumentException(
+                    FIRST);
+        }
+        this.bagCount = bagCount;
+    }
+
+    public double getBaseTipPerBag() {
+        return baseTipPerBag;
+    }
+
+    public void setBaseTipPerBag(double baseTipPerBag) {
+        if(baseTipPerBag < 0) {
+            throw new IllegalArgumentException(
+                    "error: base tip must be greater than or equal to zero");
+        }
+        this.baseTipPerBag = baseTipPerBag;
+    }
 }
--- src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -9,10 +9,54 @@ package dip.lab2.student.solution1;
  * @author User
  */
 public class FoodServiceTipCalculator implements TipCalculator{
+        private static final double MIN_BILL = 0.00;
+        private static final String BILL_ENTRY_ERR =
+            "Error: bill must be greater than or equal to " + MIN_BILL;
 
-    @Override
-    public double getTip() {
-        throw new UnsupportedOperationException("Not supported yet.");
+private double bill;
+    public enum ServiceQuality {
+        GOOD, FAIR, POOR
     }
+    private ServiceQuality serviceQuality;
+
+    public FoodServiceTipCalculator(ServiceQuality q, double billAmt) {
+        this.setServiceRating(q);
+        this.setBill(billAmt);
+    }
+
+    public double getTip() {
+        double tip = 0.00; // always initialize local variables
+
+        switch(serviceQuality) {
+            case GOOD:
+                tip = bill * GOOD_RATE;
+                break;
+            case FAIR:
+                tip = bill * FAIR_RATE;
+                break;
+            case POOR:
+                tip = bill * POOR_RATE;
+                break;
+        }
+
+        return tip;
+    }
+
+    public final void setBill(double billAmt) {
+        if(billAmt < MIN_BILL) {
+            throw new IllegalArgumentException(BILL_ENTRY_ERR);
+        }
+        bill = billAmt;
+    }
+
+    public final void setServiceRating(ServiceQuality q) {
+        // No need to validate because enums provide type safety!
+        serviceQuality = q;
+    }
+
+    public ServiceQuality getServiceQuality() {
+        return serviceQuality;
+    }
+
     
 }
